4c069b026a97bac5dc9645d075a2e72649ede906
Matias Ezequiel Noir <108021175+Mati-Noir@users.noreply.github.com>
2022-08-03 16:05:26 -0300

    No mas opciones de redireccionamiento
    Modificaciones en la url para entrar a la pagina principal sin tener que entrar a la pagina de opciones a elegir

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -9,6 +9,7 @@ from mi_app import views
 
 
 urlpatterns = [
+    path('', mostrar_index),
     path('mi-pagina/', mostrar_index, name = 'pagina-inicial'),
     path('listar-cursos/', views.listar_cursos, name="listar-cursos"),
     path('listar-estudiantes/', views.listar_estudiantes, name="listar-estudiantes"),

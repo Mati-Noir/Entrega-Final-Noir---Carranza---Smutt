9466d21d3a72c982af24f5e43d41fed67f819649
Matias Ezequiel Noir <108021175+Mati-Noir@users.noreply.github.com>
2022-08-13 00:02:48 -0300

    EDIT

--- mi_app/forms.py
+++ mi_app/forms.py
@@ -2,13 +2,13 @@ from dataclasses import fields
 from doctest import master
 from tkinter import Widget
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 
 
 
-
+# ---- Sector formularios del usuario y su perfil ---- #
 
 
 class userRegisterForm(UserCreationForm):
@@ -23,6 +23,22 @@ class userRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name']
         help_texts = {k:'' for k in fields}
 
+
+        
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    is_active = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={'class': 'form-check'}))
+    #date_joined = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'last_login', 'is_superuser', 'is_staff', 'is_active')
 
 
 # ---- Sector formularios de las reseñas ---- #
